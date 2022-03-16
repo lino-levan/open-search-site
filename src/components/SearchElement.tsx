7e4a364deb91053ac3fb5fe4233d04ae4252cb61
lino-levan <11367844+lino-levan@users.noreply.github.com>
2022-03-16 08:22:58 -0700

    fixed hover

--- src/components/SearchElement.tsx
+++ src/components/SearchElement.tsx
@@ -11,11 +11,13 @@ function SearchElement({result}: {result: ISearch}) {
   const [hover, setHover] = useState(false)
 
   return (
-    <div className="w-full p-2" key={result.href} onMouseOver={()=>setHover(true)} onMouseOut={()=>setHover(false)}>
-      <a href={result.href}>
-        <p className='text-slate-400 hover:text-slate-700'>{result.href}</p>
-        <h2 className='text-blue-500 text-xl hover:text-blue-800'>{result.title}</h2>
-      </a>
+    <div className="w-full p-2" key={result.href}>
+      <div className="w-min">
+        <a href={result.href} onMouseOver={()=>setHover(true)} onMouseOut={()=>setHover(false)}>
+          <p className='text-slate-400 hover:text-slate-700'>{result.href}</p>
+          <h2 className='text-blue-500 text-xl hover:text-blue-800'>{result.title}</h2>
+        </a>
+      </div>
       <div className={`absolute shadow fade transition-all pointer-events-none ${hover? 'opacity-100' : 'opacity-0'}`}>
         <img src={result.screenshot} width={192} height={108}></img>
       </div>
