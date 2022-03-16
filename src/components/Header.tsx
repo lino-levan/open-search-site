2ef285961faa0fdde0b06fb75296934bea4308a4
lino-levan <11367844+lino-levan@users.noreply.github.com>
2022-03-16 08:15:40 -0700

    Added images tab

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,23 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
+import useQuery from '../lib/useQuery'
 import SearchBar from './SearchBar'
 
 function Header() {
+  const query = useQuery()
+
   return (
-    <div className='flex items-center p-5 shadow'>
-      <Link to="/"><h1 className='schoolbell text-emerald-400 text-4xl mr-10'>OpenSearch</h1></Link>
-      <SearchBar />
-    </div>
+    <>
+      <div className='flex items-center p-5 shadow'>
+        <Link to="/"><h1 className='schoolbell text-emerald-400 text-4xl mr-10'>OpenSearch</h1></Link>
+        <SearchBar />
+      </div>
+      <div className='flex justify-center'>
+        <div className=' flex items-start p-2 max-w-3xl w-full'>
+          <NavLink to={`/search?q=${query.get('q')}`} className={isActive => "p-2" + (isActive ? " text-blue-500" : "")}>All</NavLink>
+          <NavLink to={`/images?q=${query.get('q')}`} className={isActive => "p-2" + (isActive ? " text-blue-500" : "")}>Images</NavLink>
+        </div>
+      </div>
+    </>
   )
 }
 
